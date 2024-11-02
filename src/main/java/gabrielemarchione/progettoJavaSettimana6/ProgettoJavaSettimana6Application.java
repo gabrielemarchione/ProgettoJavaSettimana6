973abf789c2e9305fc6bd04724a26ccdd4f1e1d5
Gabriele <gabrielegnr@gmail.com>
2024-11-03 00:01:51 +0100

    base
    base +env prop

---
+++ src/main/java/gabrielemarchione/progettoJavaSettimana6/ProgettoJavaSettimana6Application.java
@@ -0,0 +1,13 @@
+package gabrielemarchione.progettoJavaSettimana6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoJavaSettimana6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoJavaSettimana6Application.class, args);
+	}
+
+}
